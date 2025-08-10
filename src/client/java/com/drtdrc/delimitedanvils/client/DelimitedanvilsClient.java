00253f503d81f56c55e5ba10e5f066b8af6d9d7e
Christian Deardorff <35127565+crdtrd@users.noreply.github.com>
2025-08-09 19:32:53 -0700

    Initial commit

---
+++ src/client/java/com/drtdrc/delimitedanvils/client/DelimitedanvilsClient.java
@@ -0,0 +1,10 @@
+package com.drtdrc.delimitedanvils.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DelimitedanvilsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/drtdrc/delimitedanvils/Delimitedanvils.java
@@ -0,0 +1,10 @@
+package com.drtdrc.delimitedanvils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Delimitedanvils implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
